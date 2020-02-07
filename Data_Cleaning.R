02e1f82117ef64ccc6f6b3bac0bcff5dedfd22f8
Erik Guterman <erikguterman@wbk-er-10-132-116-110.wireless.umn.edu>
2020-02-07 15:55:13 -0600

    Cleaned WI data further, bound together into year-level DFs

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -3,7 +3,8 @@ library(readxl)
 library(magrittr)
 
 ##minnesota is nice and easy, the warnings relate to columns we don't use anyway so don't need to pay attention to them
-MN_results <- dir() %>% str_subset("MN_") %>% lapply(read_excel, sheet = "Results") 
+MN_results <-
+  dir() %>% str_subset("MN_") %>% lapply(read_excel, sheet = "Results")
 
 WI_clean_function <- function(sheetIDs, list)
 {
@@ -16,35 +17,71 @@ WI_clean_function <- function(sheetIDs, list)
         sheet = x,
         skip = 9,
         .name_repair = "unique"
-        )
+      )
     })
   }
   return(list)
 }
+WI_rowcleaning <- function(df)
+{
+  if (ncol(df) == 7)
+  {
+    df <- add_column(df, temp = NA, .after = 5)
+  }
+  if (ncol(df) == 8)
+  {
+    df <- add_column(df, temp1 = NA, .after = 5)
+  }
+  if (ncol(df) == 9)
+  {
+    df <- add_column(df, temp2 = NA, .after = 5)
+  }
+  if (ncol(df) == 10)
+  {
+    df <- add_column(df, temp3 = NA, .after = 5)
+  }
+  return(df)
+}
 
 district_IDs <- paste(rep("District"), 1:8)
 WI_sheets <- dir() %>% str_subset("WI_")
-WI_col_names <- c("County", "Ward", "Total Votes", "Republican", "Democrat", "temp", "Independent", "Scattering")
+WI_col_names <-
+  c(
+    "County",
+    "Ward",
+    "Total Votes",
+    "Republican",
+    "Democrat",
+    "Independent",
+    "Independent 2", 
+    "temp",
+    "Independent",
+    "Scattering", 
+    "District"
+  )
 
 WI_results <- list()
-WI_results <- WI_clean_function(WI_sheets, WI_results) 
+WI_results <- WI_clean_function(WI_sheets, WI_results)
 ##adding the district factor separately, for unclear reasons Map won't work within function written above
-WI_results <- lapply(WI_results, function(x){Map(cbind, x, District = district_IDs)})
+WI_results <-
+  lapply(WI_results, function(x) {
+    Map(cbind, x, District = district_IDs)
+  })
 
+##cleans dataframes for WI in order to make them the same length (makes it easier to merge later)
+##this happens when there's multiple independents tracked
+##and then bind all districts together so it's just one df per year
 
-
-##next time, go through each of the nested DFs in WI, change them to a matching number of columns (b/c there's some with 0 INDs, some with 1/2)
-##and then change the column names to match in order to allow merging all into one
-
-for(i in 1:8)
+for (i in 1:4)
 {
-  if(ncol(WI_results[[i]]) == 7)
-  {
-    WI_results[[i]] <- add_column(WI_results[[i]], NA, .after = 5)
-  }
+  WI_results[[i]] <- lapply(WI_results[[i]], WI_rowcleaning)
+  WI_results[[i]] <- lapply(WI_results[[i]], setNames, nm = WI_col_names)
+  WI_results[[i]] <- bind_rows(WI_results[[i]])
 }
 
+
+
 ##use this to remove county and total values, in order to avoid double counting
-#WI_results  %<>% bind_rows() %>% filter((!str_detect(County, "Total") | is.na(County)) & (!str_detect(Ward, "Total")))
+WI_results  %<>% filter((!str_detect(County, "Total") | is.na(County)) & (!str_detect(Ward, "Total")))
 
 
